412e4d8f915ea19e74ed96edc662071bf418694b
VladimirYugay <voviktyl@gmail.com>
2025-04-04 15:20:32 +0200

    Add parallel and non parallel versions

--- run_eval_parallel.sh
+++ run_eval_parallel.sh
@@ -2,16 +2,24 @@
 
 BASE_DIR="/home/vy/datasets/ScanNetVideos"
 OUTPUT_DIR="test"
+MAX_JOBS=4
+
+job_count=0
 
 for imagedir in "$BASE_DIR"/*/; do
   scene_name=$(basename "$imagedir")
-
-  echo "Processing scene: $scene_name"
-  echo "$imagedir"
+  echo "Launching: $scene_name"
 
   python main/eval_vlom.py \
     --config-path=../configs \
     --config-name=scannet \
     data.imagedir="$imagedir" \
-    data.savedir="$OUTPUT_DIR/$scene_name"
+    data.savedir="$OUTPUT_DIR/$scene_name" &
+
+  ((job_count++))
+  if (( job_count % MAX_JOBS == 0 )); then
+    wait
+  fi
 done
+
+wait
